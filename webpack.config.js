701d37352486d5a52fdcdd4bae24d86e9c6dd0be
Cory Boyd <cory@grooveapp.com>
2018-03-17 11:53:16 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    background: './src/background/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new CopyWebpackPlugin([
+      { from: './src/manifest.json' },
+      { from: './src/background.html' },
+    ])
+  ]
+};

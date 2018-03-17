630bc0d34d324d58779634ee0c790d8c58ea226f
Cory Boyd <cory@grooveapp.com>
2018-03-17 16:32:13 -0700

    Render into iframe

--- src/contentScript/ui/App/index.js
+++ src/contentScript/ui/App/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from 'material-ui/styles';
 
 import Typography from 'material-ui/Typography';
 
@@ -9,13 +13,42 @@ const styles = {
 
 @withStyles(styles)
 export default class App extends Component {
+  static contextTypes = {
+    window: PropTypes.any,
+    document: PropTypes.any,
+  };
+
+  generateClassName = createGenerateClassName();
+  jss = null;
+
+  componentWillMount() {
+    const { window, document } = this.context;
+
+    console.log('window', window);
+    console.log('document', document);
+
+    const styleNode = document.createComment("jss-insertion-point");
+    document.head.insertBefore(styleNode, document.head.firstChild);
+
+    this.jss = create(jssPreset());
+    this.jss.options.insertionPoint = styleNode;
+
+    console.log('this.jss', this.jss);
+  }
+
   render() {
     const { classes } = this.props;
 
     return (
-      <div className={classes.root}>
-        <Typography variant="heading-1">Hello, React!</Typography>
-      </div>
+      <JssProvider jss={this.jss} generateClassName={this.generateClassName}>
+        <div className={classes.root}>
+          <Typography
+            variant="display1"
+          >
+            Hello, React!
+          </Typography>
+        </div>
+      </JssProvider>
     );
   }
 }
--- src/contentScript/ui/index.js
+++ src/contentScript/ui/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ReactFrame from 'react-frame-component';
 
 import App from './App';
+
+const rootNode = document.createElement('div');
+document.body.appendChild(rootNode);
 
 export function initialize () {
   console.log('initialize UI');
 
-  render(<App />, document.body);
+  render(
+    (
+      <ReactFrame>
+        <App />
+      </ReactFrame>
+    ),
+    rootNode,
+  );
 }

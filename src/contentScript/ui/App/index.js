81352f249bb2d5d9cb5935a89502a9a56c11db8a
Cory Boyd <cory@grooveapp.com>
2018-03-17 18:03:52 -0700

    Remove silly resizing on timer

--- src/contentScript/ui/App/index.js
+++ src/contentScript/ui/App/index.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import classNames from 'classnames';
 
 import { blue } from 'material-ui/colors'
 import Typography from 'material-ui/Typography';
@@ -12,13 +11,7 @@ const styles = theme => ({
     width: 400,
     boxShadow: theme.shadows[1],
     background: blue[300],
-    transition: '250ms',
   },
-  bigger: {
-    width: 500,
-    height: 500,
-    boxShadow: theme.shadows[3],
-  }
 });
 
 @withStyles(styles)
@@ -27,19 +20,14 @@ export default class App extends PureComponent {
     setAppRootNode: PropTypes.func.isRequired,
   };
 
-  state = {
-    bigger: false,
-  };
-
   componentDidMount() {
-    // Give parent reference to the actual application root
+    // Give parent reference to the actual application root.
+    //
+    // This is the only needed magic sauce to magically resize the iframe based off of the size of
+    // the content within it.
     const { setAppRootNode } = this.props;
 
     setAppRootNode(this.appRootNode);
-
-    setTimeout(() => {
-      setInterval(() => this.setState({ bigger: !this.state.bigger }), 1000);
-    }, 2000);
   }
 
   render() {
@@ -49,10 +37,7 @@ export default class App extends PureComponent {
     return (
       <div
         ref={(node) => { this.appRootNode = node; }}
-        className={classNames({
-          [classes.root]: true,
-          [classes.bigger]: this.state.bigger,
-        })}
+        className={classes.root}
       >
         <Typography
           variant="display1"

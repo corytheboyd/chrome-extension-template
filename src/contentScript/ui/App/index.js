1e73f329467c5419a1e6098bc3f4b1a30f04756a
Cory Boyd <cory@grooveapp.com>
2018-03-17 19:22:41 -0700

    Redux demo

--- src/contentScript/ui/App/index.js
+++ src/contentScript/ui/App/index.js
@@ -1,9 +1,14 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import { connect } from 'react-redux';
 
 import { blue } from 'material-ui/colors'
+import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
+
+import { initialize } from '../../redux/actions/app';
+import { isInitialized } from '../../redux/selectors/app';
 
 const styles = theme => ({
   root: {
@@ -14,9 +19,24 @@ const styles = theme => ({
   },
 });
 
+@connect(
+  (state) => ({
+    isInitialized: isInitialized(state),
+  }),
+  {
+    initialize,
+  },
+)
 @withStyles(styles)
 export default class App extends PureComponent {
   static propTypes = {
+    // Redux state
+    isInitialized: PropTypes.bool.isRequired,
+
+    // Redux action dispatch
+    initialize: PropTypes.func.isRequired,
+
+    // React props
     setAppRootNode: PropTypes.func.isRequired,
   };
 
@@ -34,6 +54,12 @@ export default class App extends PureComponent {
     // HOC
     const { classes } = this.props;
 
+    // Redux state
+    const { isInitialized } = this.props;
+
+    // Redux action dispatch
+    const { initialize } = this.props;
+
     return (
       <div
         ref={(node) => { this.appRootNode = node; }}
@@ -44,6 +70,23 @@ export default class App extends PureComponent {
         >
           Hello, world!
         </Typography>
+
+        {isInitialized && (
+          <Typography
+            variant="display2"
+          >
+            WE ARE FULLY INITIALIZED
+          </Typography>
+        )}
+
+        <Button
+          disabled={isInitialized}
+          onClick={() => initialize()}
+          variant="raised"
+          color="primary"
+        >
+          Initialize
+        </Button>
       </div>
     );
   }
